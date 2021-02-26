978fdca3ddea6f4139e1f6b85d82a1dc197b49d1
danielfox22 <77760833+danielfox22@users.noreply.github.com>
2021-02-26 10:09:44 -0500

    Add files via upload

---
+++ week7/src/App.js
@@ -0,0 +1,13 @@
+import Pizza from './components/Pizza';
+import Hotdogs from './components/Hotdogs';
+function App() {
+  return (
+    <div>
+      <h1>Food Tracker</h1>
+      <Pizza/>
+      <Hotdogs/>
+    </div>
+  );
+}
+
+export default App;
